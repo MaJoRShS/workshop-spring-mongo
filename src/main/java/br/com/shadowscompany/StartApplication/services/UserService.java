c3e22d35670fca3627b7307ddbc1424a8d89ac52
MaJoRShS <lukas.mr@hotmail.com>
2021-11-08 21:56:06 -0300

    create the service and the method to recorver all user

---
+++ src/main/java/br/com/shadowscompany/StartApplication/services/UserService.java
@@ -0,0 +1,19 @@
+package br.com.shadowscompany.StartApplication.services;
+
+import br.com.shadowscompany.StartApplication.domain.User;
+import br.com.shadowscompany.StartApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
