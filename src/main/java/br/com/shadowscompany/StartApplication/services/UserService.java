0ade78ea83496330486935eb3e6358530d94d636
MaJoRShS <lukas.mr@hotmail.com>
2021-11-09 22:03:46 -0300

    Inlcude on Service the new method findById

--- src/main/java/br/com/shadowscompany/StartApplication/services/UserService.java
+++ src/main/java/br/com/shadowscompany/StartApplication/services/UserService.java
@@ -2,10 +2,12 @@ package br.com.shadowscompany.StartApplication.services;
 
 import br.com.shadowscompany.StartApplication.domain.User;
 import br.com.shadowscompany.StartApplication.repository.UserRepository;
+import br.com.shadowscompany.StartApplication.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
