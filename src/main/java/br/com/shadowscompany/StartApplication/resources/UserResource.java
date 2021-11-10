8b8ade83592540e64c2cb7a5884abdd55574a98b
MaJoRShS <lukas.mr@hotmail.com>
2021-11-09 22:04:44 -0300

    Include on Resource the new method findById and creating the treatment to not found Id on search

--- src/main/java/br/com/shadowscompany/StartApplication/resources/UserResource.java
+++ src/main/java/br/com/shadowscompany/StartApplication/resources/UserResource.java
@@ -5,9 +5,7 @@ import br.com.shadowscompany.StartApplication.dto.UserDTO;
 import br.com.shadowscompany.StartApplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,10 +19,16 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
         List<UserDTO> listDTO = list.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
