274392715b7076be3b5dad52cced03686b465f99
MaJoRShS <lukas.mr@hotmail.com>
2021-11-09 21:29:55 -0300

    Alter class to implements the UserDTO on the Source

--- src/main/java/br/com/shadowscompany/StartApplication/resources/UserResource.java
+++ src/main/java/br/com/shadowscompany/StartApplication/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.shadowscompany.StartApplication.resources;
 
 import br.com.shadowscompany.StartApplication.domain.User;
+import br.com.shadowscompany.StartApplication.dto.UserDTO;
 import br.com.shadowscompany.StartApplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
