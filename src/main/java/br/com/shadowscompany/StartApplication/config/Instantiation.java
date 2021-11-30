c3676245c7d47cfbe8713a5c5daf29ab533026ab
MaJoRShS <lukas.mr@hotmail.com>
2021-11-29 21:38:42 -0300

    Here I change the post to have one Create a new Class AuthorDTO and save the author together with post

--- src/main/java/br/com/shadowscompany/StartApplication/config/Instantiation.java
+++ src/main/java/br/com/shadowscompany/StartApplication/config/Instantiation.java
@@ -2,6 +2,7 @@ package br.com.shadowscompany.StartApplication.config;
 
 import br.com.shadowscompany.StartApplication.domain.Post;
 import br.com.shadowscompany.StartApplication.domain.User;
+import br.com.shadowscompany.StartApplication.dto.AuthorDTO;
 import br.com.shadowscompany.StartApplication.repository.PostRepository;
 import br.com.shadowscompany.StartApplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,8 +37,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou para São Paulo . Abraços!", maria);
-        Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom Dia", "Acordei Feliz Hoje!", maria);
+        Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou para São Paulo . Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom Dia", "Acordei Feliz Hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
