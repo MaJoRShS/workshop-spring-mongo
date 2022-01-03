70b04a08316c62c407a398f9c7b9aacd44c9eb9e
MaJoRShS <lukas.mr@hotmail.com>
2022-01-03 14:58:46 -0300

    Class 363
    Finished course

--- src/main/java/br/com/shadowscompany/StartApplication/resources/util/URL.java
+++ src/main/java/br/com/shadowscompany/StartApplication/resources/util/URL.java
@@ -1,12 +1,28 @@
 package br.com.shadowscompany.StartApplication.resources.util;
 
+import javax.xml.crypto.Data;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
     public static String decodeParam(String text) throws UnsupportedEncodingException {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue)  {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        }catch (ParseException e){
+            return defaultValue;
+
+        }
     }
 }
