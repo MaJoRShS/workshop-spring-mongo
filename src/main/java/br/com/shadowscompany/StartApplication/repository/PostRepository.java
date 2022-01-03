2cd4d5fe4b48c0f8efce260c4745a1b1d29153c3
MaJoRShS <lukas.mr@hotmail.com>
2022-01-03 13:57:38 -0300

    class 362 Native Query using annotation @Query

--- src/main/java/br/com/shadowscompany/StartApplication/repository/PostRepository.java
+++ src/main/java/br/com/shadowscompany/StartApplication/repository/PostRepository.java
@@ -2,12 +2,17 @@ package br.com.shadowscompany.StartApplication.repository;
 
 import br.com.shadowscompany.StartApplication.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.awt.*;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
